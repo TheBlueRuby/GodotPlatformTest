c59b446bd1a5b36066138945007c11287b0f9350
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2024-02-07 22:24:20 +1100

    added vision-based pathfinding

--- Scripts/walking-enemy.gd
+++ Scripts/walking-enemy.gd
@@ -7,9 +7,14 @@ extends CharacterBody2D
 @export var player: Node2D
 @onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
 
+@onready var player_scanner := $PlayerScanner as RayCast2D
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var can_see_player = false
+
 func _physics_process(delta):
+	
 	var dir = to_local(nav_agent.get_next_path_position()).normalized()
 	velocity.x = dir.x * speed
 	
@@ -25,4 +30,18 @@ func make_path():
 
 
 func _on_timer_timeout():
-	make_path()
+	scan_for_player()
+	if can_see_player:
+		make_path()
+
+func scan_for_player():
+	can_see_player = false
+	player_scanner.target_position = player_scanner.to_local(player.global_position)
+	
+	if player_scanner.is_colliding():
+		if player_scanner.get_collider() == player:
+			var player_dist = player_scanner.get_collision_point() - player_scanner.global_position
+			if player_dist.length() < 256:
+				can_see_player = true
+		
+
